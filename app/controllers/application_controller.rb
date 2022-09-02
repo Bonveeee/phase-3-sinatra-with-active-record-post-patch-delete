5464681fd58538672d337cbab3d1b058bc17b5b0
bonveeee <bonochieng@gmail.com>
2022-09-02 21:50:26 +0300

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,35 @@ class ApplicationController < Sinatra::Base
     })
   end
 
+  delete '/reviews/:id' do
+    # find the review using the ID
+    review = Review.find(params[:id])
+    # delete the review
+    review.destroy
+    # send a response with the deleted review as JSON
+    review.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+  # Handle requests with the PATCH HTTP verb to /reviews/:id
+  # Find the review to update using the ID
+  # Access the data in the body of the request
+  # Use that data to update the review in the database
+  # Send a response with updated review as JSON
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
 end
